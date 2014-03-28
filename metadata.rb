1cd2dff99f1a7e73636a07df33a960c5a351b890
Ted Neykov <ted.neykov@rackspace.com>
2014-03-28 07:06:32 -0500

    Add 'bc' package and refactor some code. Add .rubocop.yml

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Rackspace'
 maintainer_email 'ted.neykov@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures rackops-gluster'
-#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-depends 'apt'
+depends 'rackspace_apt', '~> 3.0.3'
