e45d3e6a9067aa89417fdee12e88d7c6017bed96
Ted Neykov <ted.neykov@rackspace.com>
2014-03-28 07:10:06 -0500

    Version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       'Rackspace'
 maintainer_email 'ted.neykov@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures rackops-gluster'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'rackspace_apt', '~> 3.0.3'
